97d6e6759b651746d12189f431d0c8768785110d
Fazeel10 <f21binft1m01097@iub.edu.pk>
2023-12-19 22:39:05 +0500

    Some draft design to work on

---
+++ imgai/lib/splashscreen.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:imgai/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const BottomNavigationBarExample(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 131, 129, 245),
+            Color.fromARGB(255, 21, 0, 255)
+          ], begin: Alignment.topRight, end: Alignment.bottomLeft),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/image.png",
+              height: 100,
+              width: 100,
+            ),
+            const Text(
+              'Generate Photo',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 23, 98, 229)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
